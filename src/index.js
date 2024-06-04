744461dd2bba01134c551d002a26cbd42e73a347
Stephen <stephen@behnfeldt.pro>
2024-06-04 06:42:29 -0400

    Dev Server with hot module replacement

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const elvenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
     refinedMoonstone: 4
-}
+};
+
+const elvenGauntletRecipe = {
+    ...elvenShieldRecipe,
+    leather: 3,
+    refinedMoonstone: 2
+};
 
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletRecipe);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require( 'path' );
 
 module.exports = {
+    mode: "development",
     devServer: {
         static: './dist'
     },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve( __dirname, 'dist' )
+    }
 }
