d6351bae27bb0d2555692eb80f60c510b7a0acef
Stephen <stephen@behnfeldt.pro>
2024-06-04 08:19:45 -0400

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-const path = require( 'path' );
+const path = require('path');
 
 module.exports = {
     mode: "development",
+    devtool: false,
     devServer: {
         static: './dist'
     },
 
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    },
+
     output: {
         filename: 'bundle.js',
-        path: path.resolve( __dirname, 'dist' )
+        path: path.resolve(__dirname, 'dist')
     }
 }
